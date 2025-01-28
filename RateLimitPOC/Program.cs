f1e5afd1a6622a472a442407afb209ec6e8a922d
Dieter Volker <dieterv@badgerholdings.co.za>
2025-01-28 16:34:03 +0200

    Add project files.

---
+++ RateLimitPOC/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace RateLimitPOC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var logger = new Logger();
+
+            try
+            {                
+                for (int i = 1; i <= 13; i++)
+                {
+                    
+                   logger.WriteLogAsync($"Hello, World! {i}").Wait();
+                }
+
+                Console.WriteLine("Press any key to exit.");
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+            
+        }
+    }
+}
---
+++ RateLimitPOC/TextWriter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Globalization;
+using System.Threading.RateLimiting;
+using System.Threading.Tasks;
+
+public class Logger
+{
+    private readonly RateLimiter _rateLimiter;
+
+    public Logger()
+    {
+        //configure the ratelimiter options
+        var options = new FixedWindowRateLimiterOptions
+        {
+            PermitLimit = 7, // Allow 7 operations
+            Window = TimeSpan.FromSeconds(10), // Within a 10-second window
+            AutoReplenishment = true, // Automatically replenish permits
+            QueueProcessingOrder = QueueProcessingOrder.OldestFirst // Use a FIFO queue
+        };
+
+        _rateLimiter = new FixedWindowRateLimiter(options);
+    }
+
+    
+
+    public async Task WriteLogAsync(string message)
+    {
+        RateLimitLease lease;
+        
+        //loop until we acquire a lease
+        do
+        {
+            lease = await _rateLimiter.AcquireAsync(1);
+            if (!lease.IsAcquired)
+            {
+                if (lease.TryGetMetadata(MetadataName.RetryAfter, out var retryAfter))
+                {
+                    Console.WriteLine($"Rate limit exceeded. Retry after {retryAfter.Seconds.ToString(NumberFormatInfo.InvariantInfo)} seconds");
+                }
+                await Task.Delay(1000); // Wait for a short period before retrying
+            }
+        } while (!lease.IsAcquired);
+
+        //execute the log operation
+        using (lease)
+        {
+            string logEntry = $"{DateTime.Now}: {message}{Environment.NewLine}";
+
+            Console.WriteLine(logEntry);
+        }        
+    }
+}
